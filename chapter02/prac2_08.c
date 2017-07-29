bd7d7520ae54a0ece1e5538c9d4796db1d5fbac8
zhaoyining <zyn0212@126.com>
2017-07-30 07:59:55 +0800

    advanced chapter02-08

--- chapter02/prac2_08.c
+++ chapter02/prac2_08.c
@@ -2,11 +2,13 @@
 static int rightrot(int x, int n);
 int main(void)
 {
-	printf("%0#x\n", rightrot(15, 3));
+	printf("%0#x\n", rightrot(15, 2));
 	return 0;
 }
 static int rightrot(int x, int n)
 {
+	if( n < 1 || n > (sizeof(int) << 3) - 1 )
+		return x;
 	int tmp = (~(~0 << n) & x) << (sizeof(int) << 3) - n;
 	return x >> n | tmp;
 }
