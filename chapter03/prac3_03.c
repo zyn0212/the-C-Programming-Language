18cdd9787dff0c91e05382b3180b5953eb389e9d
zhaoyining <zyn0212@126.com>
2017-08-03 20:19:36 +0800

    advance chapter3-03

--- chapter03/prac3_03.c
+++ chapter03/prac3_03.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
 #define MAX 100
 static char *expand(char *s1, char *s2);
 int main(int argc, char *argv[])
 {
-	char s1[MAX] = "a-r-cz-";
+	char s1[MAX] = "a-d-0-8-z-";
 	char s2[MAX << 1] = "fff\0";
 	printf("%s\n", expand(s1, s2));
 	return 0;
 }
 static char *expand(char *s1, char *s2)
 {
-	char *ret = s2, *loc = s1;
-	int i = 0;
-	while( '\0' != *s1 )
-	{
-		switch( *s1 )
+	char *ret = s2;
+	int c = 0;
+	while( '\0' != (c = *s1++) )
+		if( 1 == isalnum(c) && '-' == *s1 && c < *(s1 + 1) )
 		{
-			case '-':
-				if( loc == s1 || '\0' == *(s1 + 1) )
-				{
-					*s2++ = '-';
-					break;
-				}
-				for( i = *(s1 - 1) + 1; i < *(s1 + 1); ++i )
-					*s2++ = i;
-				break;
-			default:
-				*s2++ = *s1;
-				break;
+			++s1;
+			while( c < *s1 )
+				*s2++ = c++;
 		}
-		++s1;
-	}
+		else
+			*s2++ = c;
 	*s2 = '\0';
 	return ret;
 }
